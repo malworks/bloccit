719838347894e994beb40b647993b6e9d6f6a461
Mallory <malloryworks@gmail.com>
2016-12-05 15:52:23 -0800

    Checkpoint 13: Testing

---
+++ spec/controllers/welcome_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe WelcomeController, type: :controller do
+  describe "GET index" do
+    # it describes what the test does
+    it "renders the index template" do
+      # now this is what the test should do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+      expect(response).to render_template("about")
+    end
+  end
+end
