48c68164a3a3a1b1d0352450aedb98269f0a10f2
Mallory <malloryworks@gmail.com>
2016-12-18 11:38:20 -0800

    Assignment 19: CRUD

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -15,8 +15,9 @@ class AdvertisementsController < ApplicationController
   	@advertisement = Advertisement.new
     @advertisement.title = params[:advertisement][:title]
     @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params[:advertisement][:price]
 
-    if @post.save
+    if @advertisement.save
       flash[:notice] = "Ad was saved."
       redirect_to @advertisement
     else
