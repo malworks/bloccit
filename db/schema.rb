d67591b4819e760c047235b2dab790a4ce891d0a
Mallory <malloryworks@gmail.com>
2016-12-22 17:08:23 -0800

    Checkpoint 22: progress

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218172050) do
+ActiveRecord::Schema.define(version: 20161223004432) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 20161218172050) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
