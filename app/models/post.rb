d362768914052f63d3285b2fcd15cccf3e3871e5
Mallory <malloryworks@gmail.com>
2017-01-15 13:50:06 -0800

    Checkpoint 30 progress: CSS not rendering?

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,28 @@
 class Post < ApplicationRecord
-	belongs_to :topic
+  belongs_to :topic
 	belongs_to :user
-  	has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
-  	default_scope { order('created_at DESC') }
+  default_scope { order('created_at DESC') }
 
-  	scope :ordered_by_title, -> { order('title DESC') }
-  	scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
+  scope :ordered_by_title, -> { order('title DESC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 
-  	validates :title, length: { minimum: 5 }, presence: true
-  	validates :body, length: { minimum: 20 }, presence: true
-  	validates :topic, presence: true
-  	validates :user, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
 end
