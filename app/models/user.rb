bbc1d02ed6168bcbeb482bee97983a03ef0d7260
Mallory <malloryworks@gmail.com>
2017-01-15 12:06:33 -0800

    Checkpoint 29: Comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 	# callback
 	before_save { self.email = email.downcase if email.present? }
 	before_save :format_name
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
 
-  	resources :questions
+  resources :questions
 
-  	resources :advertisements
+  resources :advertisements
 
-  	resources :topics do
-  		resources :posts, except: [:index]
-      resources :sponsored_posts, except: [:index]
-  	end
+  resources :topics do
+  	resources :posts, except: [:index]
+    resources :sponsored_posts, except: [:index]
+  end
 
-  	resources :users, only: [:new, :create]
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
 
-    resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 
-    post 'users/confirm' => 'users#confirm'
+  resources :sessions, only: [:new, :create, :destroy]
 
-  	get 'about' => 'welcome#about'
+  post 'users/confirm' => 'users#confirm'
 
-  	root 'welcome#index'
+  get 'about' => 'welcome#about'
+
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
